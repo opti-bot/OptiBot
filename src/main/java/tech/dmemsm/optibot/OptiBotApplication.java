922c9a19864c449ef0d4575ac499c630bdb85669
Михаил Баринов <barinovmishka@yandex.ru>
2022-04-05 16:43:04 +0300

    Initialized Spring application

---
+++ src/main/java/tech/dmemsm/optibot/OptiBotApplication.java
@@ -0,0 +1,13 @@
+package tech.dmemsm.optibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/dmemsm/optibot/OptiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dmemsm.optibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
